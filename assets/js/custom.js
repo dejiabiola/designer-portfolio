6a0b5db8ce016ac13c7c600719bb453c1a9487e4
Adedeji Abiola <adedejiabiola56@gmail.com>
2020-07-22 20:12:58 +0100

    commit changes

---
+++ assets/js/custom.js
@@ -0,0 +1,39 @@
+
+
+
+// Tiny slider config
+var slider = tns({
+  container: '.my-slider',
+  items: 3,
+  slideBy: 'page',
+  autoplay: true,
+  autoWidth: false,
+  controls: false,
+  navPosition: 'bottom',
+  mouseDrag: true,
+  responsive: {
+    640: {
+      gutter: 20,
+      items: 1,
+      center: false
+    },
+    1000: {
+      items: 2,
+    },
+    1200: {
+      items: 3,
+    },
+  }
+
+});
+
+
+      function openNav() {
+        document.getElementById("myNav").style.height = "100%";
+      }
+      
+      function closeNav() {
+        document.getElementById("myNav").style.height = "0%";
+      }
+
+
