328eced5f303496e0363494fe6514f227dc53cef
Adedeji Abiola <adedejiabiola56@gmail.com>
2020-07-23 14:58:31 +0100

    fix mobile nav

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -31,7 +31,7 @@ var slider = tns({
 
 // Mobile navbar config
 function openNav() {
-  document.getElementById("myNav").style.height = "100%";
+  document.getElementById("myNav").style.height = "100vh";
 }
 
 function closeNav() {
