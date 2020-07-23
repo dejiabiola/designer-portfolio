d97bf59f9ecda01395d9765eb5471c5440790970
Adedeji Abiola <adedejiabiola56@gmail.com>
2020-07-23 15:28:57 +0100

    fix nav in customjs

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -29,14 +29,6 @@ var slider = tns({
 
 
 
-// Mobile navbar config
-function openNav() {
-  document.getElementById("myNav").style.height = "100vh";
-}
-
-function closeNav() {
-  document.getElementById("myNav").style.height = "0%";
-}
 
 
 
@@ -60,6 +52,16 @@ $(function(){
 });
   
 });
+
+// Mobile navbar config
+function openNav() {
+  document.getElementById("myNav").style.height = "100vh";
+}
+
+function closeNav() {
+  document.getElementById("myNav").style.height = "0%";
+}
+
   
 
 
